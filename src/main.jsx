9fb1bc39a42603c5898fe5d47a7a1ca021fb5a19
Hafidz-Second-Account <hafidzfurqonyet@gmail.com>
2024-08-28 13:58:17 +0700

    push ke rafi

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//render all application
+//render all applicationn
 root.render(
   <HelmetProvider>
     <BrowserRouter>
